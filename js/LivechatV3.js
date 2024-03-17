a83626e1b85a27bc59c0e59d0b522469708b69e7
TheDevNate <67196220+The-Developer-Nate@users.noreply.github.com>
2024-03-17 15:54:38 -0400

    Added moderation.
    finally pushed to github

--- js/LivechatV3.js
+++ js/LivechatV3.js
@@ -21,6 +21,19 @@ let panelVisible = false;
 let canSend = true;
 let sentPleaseWait = false;
 let streaming = false;
+let admin = false;
+
+// # GENERIC FUNCTIONS
+function getScriptPath(foo) { return window.URL.createObjectURL(new Blob([foo.toString().match(/^\s*function\s*\(\s*\)\s*\{(([\s\S](?!\}$))*[\s\S])/)[1]], { type: 'text/javascript' })); }
+
+// # WORKER
+const LiveWorker = new Worker(getScriptPath(function () {
+    self.addEventListener("message", function (ev) {
+        if (ev !== undefined && ev.data !== undefined) {
+            this.self.postMessage(JSON.parse(ev.data));
+        }
+    })
+}));
 
 // # PANEL
 
@@ -73,9 +86,10 @@ class Message {
      * @param {string} Content The content being sent/received.
      * @param {number} Type The type of message.
      */
-    constructor(Username, Id, Content, Type) {
+    constructor(Username, Id, messageId, Content, Type) {
         this.Username = Username;
         this.Id = Id;
+        this.messageId = messageId;
         this.Content = Content;
         this.Type = Type;
     }
@@ -88,6 +102,7 @@ class Message {
         return {
             Username: this.Username,
             Id: this.Id,
+            messageId: this.messageId,
             Content: this.Content,
             Type: this.Type
         };
@@ -99,7 +114,7 @@ class Message {
      * @returns {Message} The deserialized message object.
      */
     static Deserialize(jSON) {
-        return new Message(jSON.Username, jSON.Id, jSON.Content, jSON.Type);
+        return new Message(jSON.Username, jSON.Id, jSON.messageId, jSON.Content, jSON.Type);
     }
 }
 
@@ -167,9 +182,9 @@ function ReceiveMessage(MSG) {
             LivechatLog.innerHTML += `<font color="#CCCCCC"><p class="livechat-text-container">${message.Username}</font> started a stream</p> [<a class="livechat-text-container" href="/broadcast.html?csid=${encodeURIComponent(csid)}">Watch</a>]<br /><br />`;
         } else {
             if (message.Id == "sys-reserved") {
-                LivechatLog.innerHTML += `<font color="#FF7711"><p class="livechat-text-container">Livechat Server</p></font>: ${DOMPurify.sanitize(marked.parse(message.Content))}<br /><br />`;
+                LivechatLog.innerHTML += `<font color="#FF7711"><p class="livechat-text-container">Livechat Server</p></font>: ${DOMPurify.sanitize(marked.parse(message.Content.replace(/(<([^>]+)>)/ig, '')))}<br /><br />`;
             } else {
-                LivechatLog.innerHTML += `<font color="#CCCCCC"><p class="livechat-text-container">${message.Username}</p></font>: ${DOMPurify.sanitize(marked.parse(message.Content))}<br /><br />`;
+                LivechatLog.innerHTML += `<font color="#CCCCCC"><p class="livechat-text-container">${message.Username}</p></font>: ${DOMPurify.sanitize(marked.parse(message.Content.replace(/(<([^>]+)>)/ig, '')))}<br /><br />`;
             }
         }
     }
@@ -182,18 +197,46 @@ function ReceiveMessage(MSG) {
     }
 }
 
+let queue = "";
+
+/**
+ * Adds a message to the queued livechat log.
+ * @param {object} MSG The message being received.
+ * @returns {void}
+ */
+function QueueMessage(MSG) {
+    let message = Message.Deserialize(MSG);
+    if (message.Content != null) {
+        if (message.Type == 1) {
+            queue += `<font color="#CCCCCC"><p class="livechat-text-container" csid="${message.Id}">${message.Username}</font> started a stream</p> [<a class="livechat-text-container" href="/broadcast.html?csid=${encodeURIComponent(csid)}">Watch</a>]<br /><br />`;
+        } else {
+            if (message.Id == "sys-reserved") {
+                queue += `<font color="#FF7711"><p class="livechat-text-container" csid="${message.Id}" mid="${message.messageId}">Livechat Server</p></font>: ${DOMPurify.sanitize(marked.parse(message.Content))}<br /><br />`;
+            } else {
+                queue += `<font color="#CCCCCC"><p class="livechat-text-container" csid="${message.Id}" mid="${message.messageId}">${message.Username}</p></font>: ${DOMPurify.sanitize(marked.parse(message.Content))}<br /><br />`;
+            }
+        }
+    }
+}
+
 /**
  * Adds all old messages that were collected by the server.
  * @param {object} Cache The array of messages that were received.
  * @returns {void}
  */
 function ReceiveCachedMessages(Cache) {
-    console.log(Cache.length)
-
     LivechatLog.innerHTML = "";
     Cache.forEach(msg => {
-        ReceiveMessage(msg);
+        QueueMessage(msg);
     });
+    requestAnimationFrame(function () {
+        LivechatLog.innerHTML += queue;
+        LivechatLog.scrollTo({
+            top: LivechatLog.scrollHeight,
+            behavior: "instant",
+        });
+        queue = "";
+    })
 }
 
 function OnConnection() {
@@ -203,6 +246,10 @@ function OnConnection() {
             Data: UID()
         }));
     }
+}
+
+function OnDisconnect() {
+    LivechatLog.innerHTML = `<font color="#FF7711"><p class="livechat-text-container">Livechat Client</p></font>: You have been disconnected from the Livechat Server but don't worry, this will never be a ban!<br /><br />`
 }
 
 /**
@@ -216,28 +263,155 @@ function Crash() {
     }, 1)
 }
 
+function ReceiveAdminPacket(data) {
+    if (data[2] == UID()) {
+        switch (data[0]) {
+            case "Crash":
+                Crash();
+                break;
+            case "Disconnect":
+                socket.close();
+                break;
+        }
+    }
+}
+
 /**
  * Parse the message from the socket
- * @param {MessageEvent} ev
+ * @param {any} ev
  */
 function parseMessage(ev) {
-    let obj = JSON.parse(ev.data);
+    let obj = ev;
     switch (obj.Type) {
         case "message":
             ReceiveMessage(obj.Data);
             break;
         case "cache":
-            console.log(obj);
             ReceiveCachedMessages(obj.Data);
             break;
-        case "crash":
-            Crash();
+        case "isAdmin":
+            admin = true;
+            break;
+        case "adminPacket":
+            ReceiveAdminPacket(obj.Data);
             break;
     }
 }
 
-socket.addEventListener("message", parseMessage);
+socket.addEventListener("message", (ev) => {
+    LiveWorker.postMessage(ev.data);
+});
 socket.addEventListener("open", OnConnection)
+socket.addEventListener("close", OnDisconnect)
+
+LivechatLog.addEventListener("contextmenu", function (ev) {
+    if (admin && ev.target.getAttribute("csid")) {
+        ev.preventDefault();
+        let adminWindow = window.open("about:blank", "_BLANK", "width=600,height=600,resizable=no,popup")
+        adminWindow.document.body.innerHTML = `
+                <head>
+                    <style>
+                        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap');
+
+                        * {
+                            box-sizing: border-box;
+                            margin: 0;
+                            padding: 0;
+                            
+                            font-family: "Inter", sans-serif;
+                        }
+
+                        body {
+                            background-color: #121212;
+                            color: white;
+                        }
+
+                        .title {
+                            margin: 2vw;
+                            font-weight: 600;
+                        }
+
+                        button {
+                            padding: 5px;
+                            background-color: #363636;
+                            color: white;
+                            border: 0px solid white;
+                            border-radius: 6px;
+                        }
+
+                        .actions {
+                            margin: 2vw;
+                            padding: 6px;
+                            background-color: #242424
+                        }
+                    </style>
+                </head>
+                <body>
+                    <h3 class="title">${ev.target.innerText}<br><font size=2>UID: ${ev.target.getAttribute("csid")}</font></h3>
+                    <div class="actions">
+                        <p style="display: inline;">User Actions - </p>
+                        <button id="crashbtn">Crash</button>
+                        <button id="kickbtn">Disconnect</button>
+                    </div>
+                    <div class="actions">
+                        <p style="display: inline;">Message Actions - </p>
+                        <button id="modifybtn">Modify</button>
+                        <button id="deletebtn">Delete</button>
+                    </div>
+                </body>
+        `
+        adminWindow.addEventListener("blur", adminWindow.close)
+        adminWindow.document.getElementById("crashbtn").addEventListener("click", function () {
+            socket.send(JSON.stringify({
+                Type: "adminPacket",
+                Data: [
+                    "Crash",
+                    UID(),
+                    ev.target.getAttribute("csid")
+                ]
+            }))
+            adminWindow.close();
+        })
+        adminWindow.document.getElementById("kickbtn").addEventListener("click", function() {
+            socket.send(JSON.stringify({
+                Type: "adminPacket",
+                Data: [
+                    "Disconnect",
+                    UID(),
+                    ev.target.getAttribute("csid")
+                ]
+            }))
+            adminWindow.close();
+        })
+        adminWindow.document.getElementById("modifybtn").addEventListener("click", function() {
+            socket.send(JSON.stringify({
+                Type: "admin-modifyMessage",
+                Data: [
+                    ev.target.getAttribute("mid"),
+                    UID(),
+                    adminWindow.prompt("Please enter the new message contents.", ev.target.innerText)
+                ]
+            }))
+            adminWindow.close();
+        })
+        adminWindow.document.getElementById("deletebtn").addEventListener("click", function() {
+            socket.send(JSON.stringify({
+                Type: "admin-modifyMessage",
+                Data: [
+                    ev.target.getAttribute("mid"),
+                    UID(),
+                    `<font color="#969696">*This message has been moderated.*</font>`
+                ]
+            }))
+            adminWindow.close();
+        })
+    }
+})
+
+LiveWorker.addEventListener('message', function (ev) {
+    console.log(ev.data);
+    parseMessage(ev.data);
+})
 
 // # SENDING
 
@@ -311,3 +485,4 @@ if (!hasSetName) {
 // # NAMING
 hasSetName = localStorage["USR"] != null;
 USR = localStorage["USR"];
+
